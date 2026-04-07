dd923534f8acb338d90fe9f7233bc9af4648ad20
1alex8 <1.alex@tuta.com>
2026-04-08 00:44:54 +0200

    Übung 1 fixed

--- Teil_I/1_Lets-go/Uebungen/1_4-TestProg.cpp
+++ Teil_I/1_Lets-go/Uebungen/1_4-TestProg.cpp
@@ -5,17 +5,20 @@ int main(){
 
     int a1{0}, a2{0}, a3{0};
     int c {0};
-
+    /*
     std::cout << "Geben Sie Var 1 ein: ";
     std::cin >> a1;
     std::cout << "Geben Sie Var 2 ein: ";
     std::cin >> a2;
     std::cout << "Geben sie Var 3 ein: ";
     std:: cin >> a3;
-
+    */
+    
+    std::cout << "Geben Sie 3 variablen ein: ";
+    std::cin >> a1 >> a2 >> a3;
     c = (a1 + a2) * a3;
 
-    std::print("Das Ergebnis von ({} * {}) + {} ist : {}", a1 , a2, a3 ,c);
+    std::print("Das Ergebnis von ({} + {}) * {} ist : {}", a1 , a2, a3 ,c);
 
     return 0;
 }
