283436cacbd482b2b7106c8bb4e019f930f519d3
1alex8 <1.alex@tuta.com>
2026-04-09 18:29:07 +0200

    1_14

---
+++ Teil_I/1_Lets-go/1_14-switch-fallthrough.cpp
@@ -0,0 +1,62 @@
+/*
+Attribute werden in c++ mit 2 eckigen Klammern signalisiert: [[Attribut]];
+fallthrough: [[fallthrough]] signalisiert in einer switch anweisung das das break absichtlich weggelassen wird.
+*/
+
+#include<iostream>
+#include<print>
+
+int main(){
+
+    int zahl{-1};
+
+    char zeichen{'*'};
+
+    std::cout<<"Zeichen : ";
+    std::cin >> zeichen;
+
+    switch(zeichen){
+        case 'I':
+            [[fallthrough]];
+        case 'i':
+            zahl = 1;
+            break;
+        case 'v':
+            [[fallthrough]];
+        case 'V':
+            zahl = 5;
+            break;
+        case 'x':
+            [[fallthrough]];
+        case 'X':
+            zahl = 10;
+            break;
+        case 'l':
+            [[fallthrough]];
+        case 'L':
+            zahl = 50;
+            break;
+        case 'c':
+            [[fallthrough]];
+        case 'C':
+            zahl = 100;
+            break;
+        case 'd':
+            [[fallthrough]];
+        case 'D':
+            zahl = 500;
+            break;
+        case 'm':
+            [[fallthrough]];
+        case 'M':
+            zahl = 1000;
+            break;
+        default:
+            zahl = 0;
+    }
+
+    if(zahl > 0) std::cout << "Zahl = " << zahl << '\n';
+    else std::cout << "Keine römische Ziffer \n";
+
+    return 0;
+}
