3d5cd5fef8ea1c606d7cccb9c0c6023de2d071e0
1alex8 <1.alex@tuta.com>
2026-04-12 20:45:16 +0200

    Chapter 2 start

---
+++ Teil_I/2_Programmstrukturierung/2_1-function1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+long long fakultät (int);//Prototyp
+using namespace std;
+
+int main(){
+    int n{0};
+
+
+    cout << "Fakultätszahl?: ";
+    cin >> n;
+    cout << "Das Ergbenis ist " << fakultät(n) << endl;//aufruf
+
+    return 0;
+}
+
+long long fakultät (int zahl){
+    long long fak{1};
+    for (int i = 2; i <= zahl; i++){
+        fak *= i;
+    }
+    return fak;
+}
