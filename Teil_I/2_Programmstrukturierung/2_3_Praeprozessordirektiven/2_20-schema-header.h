00c48c1cb3b5d1fb4eb139cebd61d4ab7c770e86
1alex8 <1.alex@tuta.com>
2026-04-18 01:56:36 +0200

    header schemas

---
+++ Teil_I/2_Programmstrukturierung/2_3_Praeprozessordirektiven/2_20-schema-header.h
@@ -0,0 +1,20 @@
+//Schema für den Aufbau einer Headerdatei Namens: fn.h
+// Um stets eindeutige Namen zu gewährleisten,
+// Empfielt es sich die Ableitung aus dem Dateinamen.
+// Üblich ist die Hervorhebung durch Großschreibung.
+
+#ifndef FN_H
+#define FN_H
+//hier stehen die Deklarationen. z.B.
+/*
+void func_fn1(int);
+enum class Farbtyp{
+    rot,
+    gelb,
+    blau,
+    grün
+};
+
+*/
+
+#endif
---
+++ Teil_I/2_Programmstrukturierung/2_3_Praeprozessordirektiven/2_21-advanced-schema-header.h
@@ -0,0 +1,22 @@
+//Schema für den Aufbau einer Headerdatei Namens: fn.h
+// Um stets eindeutige Namen zu gewährleisten,
+// Empfielt es sich die Ableitung aus dem Dateinamen.
+// Üblich ist die Hervorhebung durch Großschreibung.
+// Bei prof. Projekten wird oft der Projektname,
+// oder der Teilprojektname eingebunden
+
+#ifndef PROJEKTX_FN_H
+#define PROJEKTX_FN_H
+//hier stehen die Deklarationen. z.B.
+/*
+void func_fn1(int);
+enum class Farbtyp{
+    rot,
+    gelb,
+    blau,
+    grün
+};
+
+*/
+
+#endif //PROJEKTX_FN_H
