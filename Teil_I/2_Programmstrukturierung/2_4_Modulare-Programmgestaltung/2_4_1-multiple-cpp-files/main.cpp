a2290f5c28eb6e60132e1710583d27983b51ed46
1alex8 <1.alex@tuta.com>
2026-04-18 18:13:19 +0200

    first multiple file project

--- Teil_I/2_Programmstrukturierung/2_4_Modulare-Programmgestaltung/2_4_1-multiple-cpp-files/main.cpp
+++ Teil_I/2_Programmstrukturierung/2_4_Modulare-Programmgestaltung/2_4_1-multiple-cpp-files/main.cpp
@@ -13,3 +13,11 @@ int main(){
     std::vector<double> daten {1.0, 1.2, 99.001, -23.5, 10.07};
     std::cout << "Der Mittelwert von daten ist: " << mittelwert(daten) << '\n';
 }
+
+
+//Befehl + Ausgabe:
+/*
+\2_4_1-multiple-cpp-files> g++ --std=c++23 main.cpp mittelwert.cpp -o middle
+\2_4_Modulare-Programmgestaltung\2_4_1-multiple-cpp-files> ./middle          
+Der Mittelwert von daten ist: 17.5542
+*/
